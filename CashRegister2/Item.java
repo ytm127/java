354dbb9f18081a2dab228e0d7677b277654c6b24
Thomas Morita <tym479@gmail.com>
2019-06-23 15:48:02 -0500

    user input part one finished

---
+++ CashRegister2/Item.java
@@ -0,0 +1,16 @@
+class Item {
+    // define Item here
+    int quantity;
+    String name;
+    double price;
+
+    public Item() {
+        name = "";
+        quantity = 0;
+        price = 0.00;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ CashRegister2/userInput.java
@@ -0,0 +1,61 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.Reader;
+import java.lang.Math;
+
+class userInput {
+    static double total;
+    static ArrayList<Item> items = new ArrayList<Item>();
+    static Item newItem;
+
+    public static void main(String[] args) {
+        String name, keepGoing;
+        double price;
+        int quantity;
+        Item newItem;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter name of item: ");
+        name = input.next();
+        System.out.println("Enter the price: ");
+        price = input.nextDouble();
+        System.out.println("Enter the quantity: ");
+        quantity = input.nextInt();
+        newItem = new Item();
+        newItem.name = name;
+        newItem.price = price;
+        newItem.quantity = quantity;
+        items.add(newItem);
+
+        // add to the total price
+        total += price * quantity;
+
+        System.out.println("Finished? y/n");
+        keepGoing = input.next();
+
+        while (keepGoing.equals("n")) {
+            System.out.println("Enter name of item: ");
+            name = input.next();
+            System.out.println("Enter the price:");
+            price = input.nextDouble();
+            System.out.println("Enter the quantity:");
+            quantity = input.nextInt();
+            newItem = new Item();
+            newItem.name = name;
+            newItem.price = price;
+            newItem.quantity = quantity;
+            items.add(newItem);
+            // add to the total price
+            total += price * quantity;
+
+            System.out.println("Finished? y/n");
+            keepGoing = input.next();
+        }
+        System.out.println("Number of items:");
+        System.out.println(items.size());
+        System.out.println("Your grand total:");
+        System.out.println("$" + total);
+
+    }
+}
