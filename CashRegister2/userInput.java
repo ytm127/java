0b00c0772674e1208b67b2f1698fb5ab35aad81f
Thomas Morita <tym479@gmail.com>
2019-06-24 15:01:09 -0500

    cleaner implementation of item input using Do-While loop

--- CashRegister2/userInput.java
+++ CashRegister2/userInput.java
@@ -16,42 +16,27 @@ class userInput {
         Item newItem;
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Enter name of item: ");
-        name = input.next();
-        System.out.println("Enter the price: ");
-        price = input.nextDouble();
-        System.out.println("Enter the quantity: ");
-        quantity = input.nextInt();
-        newItem = new Item();
-        newItem.name = name;
-        newItem.price = price;
-        newItem.quantity = quantity;
-        items.add(newItem);
-
-        // add to the total price
-        total += price * quantity;
-
-        System.out.println("Finished? y/n");
-        keepGoing = input.next();
-
-        while (keepGoing.equals("n")) {
+        do {
             System.out.println("Enter name of item: ");
             name = input.next();
-            System.out.println("Enter the price:");
+            System.out.println("Enter the price: ");
             price = input.nextDouble();
-            System.out.println("Enter the quantity:");
+            System.out.println("Enter the quantity: ");
             quantity = input.nextInt();
             newItem = new Item();
             newItem.name = name;
             newItem.price = price;
             newItem.quantity = quantity;
             items.add(newItem);
+
             // add to the total price
             total += price * quantity;
 
             System.out.println("Finished? y/n");
             keepGoing = input.next();
-        }
+
+        } while (keepGoing.equals("n"));
+
         System.out.println("\n=================\n");
         // show all the items
         items.forEach((temp) -> {
@@ -59,9 +44,9 @@ class userInput {
         });
         System.out.println("\n\nSub total:   $" + df2.format(total));
         System.out.println("Sales tax in " + location + " is " + (tax * 100) + "%");
-        System.out.println("Tax:   $" + (total * tax));
+        System.out.println("Tax:   $" + df2.format((total * tax)));
         grandTotal = total + (total * tax);
-        System.out.println("Total:   $" + grandTotal);
+        System.out.println("Total:   $" + df2.format(grandTotal));
 
     }
 }
